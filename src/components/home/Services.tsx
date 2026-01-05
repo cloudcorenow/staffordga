bde721836f4473b1e129f7cad551c8055e4464a5
cloudcorenow <138939871+cloudcorenow@users.noreply.github.com>
2026-01-05 09:37:48 -0800

    Updated Services.tsx

--- src/components/home/Services.tsx
+++ src/components/home/Services.tsx
@@ -47,7 +47,7 @@ const Services: React.FC = () => {
     {
       icon: <FileText size={40} />,
       title: "Legal Collections",
-      description: "When necessary, our network of attorneys can pursue legal action to recover debts through litigation and judgments."
+      description: "If a resolution cannot be reached through standard collection efforts, we may pursue legal action where authorized or recommend the account to our client for                        possible legal action through our attorney network."
     },
     {
       icon: <LineChart size={40} />,
